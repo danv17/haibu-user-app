4d735262c5e4c5a528f6b0fb160f5bf69268fe1a
Daniel Ariz <daniel.ariz.s@gmail.com>
2019-01-19 15:56:50 -0300

    User class with attributes

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,22 @@
+import { Address } from './address';
 export class User {
+    activo: boolean;
+    apellido: string;
+    direccion: Address;
+    fechaNacimiento: Date;
+    id: number;
+    nombre: string;
+    rut: string;
+    telefono: number;
+
+    constructor(userResponse?: any) {
+        this.activo = userResponse.activo;
+        this.apellido = userResponse.apellido;
+        this.direccion = userResponse.direccion;
+        this.fechaNacimiento = userResponse.fechaNacimiento;
+        this.id = userResponse.id;
+        this.nombre = userResponse.nombre;
+        this.rut = userResponse.rut;
+        this.telefono = userResponse.telefono;
+    }
 }
