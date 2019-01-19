4325ccbafbc782cd73e3258db6ee13b4620a809a
Daniel Ariz <daniel.ariz.s@gmail.com>
2019-01-19 16:05:05 -0300

    User services to get data from url

---
+++ src/app/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+
+const API_URL = environment.apiURL;
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  currentUSer: User;
+  constructor(private http: HttpClient) { }
+
+  getDate() {
+    return this.http.get(`${API_URL}`);
+  }
+}
